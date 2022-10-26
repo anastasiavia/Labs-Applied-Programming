6a9040a80083c4114fe4078df4dc930c00c398c5
thelenkaaa <olena.morozevych.kn.2021@lpnu.ua>
2022-10-26 17:29:19 +0300

    moro lox lab6

---
+++ check_models.py
@@ -0,0 +1,120 @@
+from schemas import *
+from manager import DBManager
+import datetime
+
+
+if __name__ == '__main__':
+    #creation of db session
+    db = DBManager()
+    session = db.session()
+
+
+    item = Item(
+        iditem=2,
+        name='notepad',
+        amount=8,
+        price=300,
+        photoUrls="htttcar/skak.a/ahksk",
+        status="available",
+        category="uni stuff"
+    )
+
+    item1 = Item(
+        iditem=3,
+        name='pencil',
+        amount=8,
+        price=300,
+        photoUrls="htttcar/skak.a/ahksk",
+        status="available",
+        category="uni stuff"
+    )
+
+
+    address = Address(
+        idaddress=2,
+        street="Korolenka",
+        city="Lviv",
+        house_number=1
+    )
+
+    address1 = Address(
+        idaddress=3,
+        street="Korolenka",
+        city="Lviv",
+        house_number=6
+    )
+
+
+    user = User(
+        iduser=2,
+        username='sashaa',
+        firstname="Sasha",
+        lastname="Kylie",
+        email="sasha@gmail.com",
+        password="1238220",
+        phone="_380679547215",
+        address_id=2
+    )
+
+    user1 = User(
+        iduser=3,
+        username='aniaa',
+        firstname="Ania",
+        lastname="Kylie",
+        email="sasha@gmail.com",
+        password="1238220",
+        phone="_380679547215",
+        address_id=3
+    )
+
+
+    order = Order(
+        idorder=2,
+        quantity=6,
+        orderDate=datetime.date(2022, 5, 2),
+        status="packed",
+        payment_method="later",
+        item_id=2,
+        user_id=2,
+    )
+
+    order1 = Order(
+        idorder=3,
+        quantity=6,
+        orderDate=datetime.date(2022, 6, 2),
+        status="packed",
+        payment_method="later",
+        item_id=3,
+        user_id=3,
+    )
+
+    order2 = Order(
+        idorder=4,
+        quantity=6,
+        orderDate=datetime.date(2022, 6, 2),
+        status="packed",
+        payment_method="later",
+        item_id=2,
+        user_id=2,
+    )
+
+    session.add(item)
+    session.add(item1)
+    session.commit()
+
+    session.add(address)
+    session.add(address1)
+    session.commit()
+
+    session.add(user)
+    session.add(user1)
+    session.commit()
+
+    session.add(order)
+    session.add(order1)
+    session.add(order2)
+    session.commit()
+
+
+
+    session.close()
---
+++ schemas.py
@@ -0,0 +1,53 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+
+
+class Address(Base):
+  __tablename__ = "address"
+  idaddress = Column(Integer, primary_key=True)
+  street = Column(String(45), nullable=False)
+  city = Column(String(45), nullable=False)
+  house_number = Column(Integer, nullable=True)
+
+
+class User(Base):
+  __tablename__ = 'user'
+  iduser = Column(Integer, primary_key=True)
+  username = Column(String(45), nullable=False)
+  firstname = Column(String(45), nullable=False)
+  lastname = Column(String(45), nullable=False)
+  email = Column(String(45), nullable=False)
+  password = Column(String(45), nullable=False)
+  phone = Column(String(45), nullable=False)
+  address_id = Column(Integer, ForeignKey("address.idaddress"))
+
+  addressUser = relationship(Address, backref='user', lazy="joined", foreign_keys=[address_id])
+
+
+class Item(Base):
+  __tablename__ = "item"
+  iditem = Column(Integer, primary_key=True)
+  name = Column(String(45), nullable=False)
+  amount = Column(Integer, nullable=True)
+  price = Column(Integer, nullable=True)
+  photoUrls = Column(String(45), nullable=False)
+  status = Column(String(45), nullable=False)
+  category = Column(String(45), nullable=False)
+
+
+class Order(Base):
+  __tablename__ = "order"
+
+  idorder = Column(Integer, primary_key=True)
+  quantity = Column(Integer, nullable=True)
+  orderDate = Column(DateTime, nullable=False)
+  status = Column(String(45), nullable=False)
+  payment_method = Column(String(45), nullable=False)
+  user_id = Column(Integer, ForeignKey("user.iduser"))
+  item_id = Column(Integer, ForeignKey("item.iditem"))
+
+  item_order = relationship(Item, backref='order', lazy="joined", foreign_keys=[item_id])
+  user_order = relationship(User, backref='order', lazy="joined", foreign_keys=[user_id])
